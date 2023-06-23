a293b79057b13a5550af25d6b26fe92c13dd7ff0
Susan Zhou <szhou089@gmail.com>
2023-06-22 23:48:36 -0700

    adding reverse2() that reverses string using builtin function

---
+++ Ex_Files_Java_Algorithms/Exercise Files/01/01_07/start/Algorithms/src/AlgorithmsTest.java
@@ -0,0 +1,35 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlgorithmsTest {
+
+    @org.junit.jupiter.api.Test
+    void reverse2() {
+
+        // Act
+        String testCaseOne = "cheese";
+        String testCaseTwo = "World";
+        String testCaseThree = "Bears!";
+        String testCaseFour = "";
+        String testCaseFive = null;
+
+        String expectedOne = "eseehc";
+        String expectedTwo = "dlroW";
+        String expectedThree = "!sraeB";
+        String expectedFour = "";
+        String expectedFive = null;
+
+        // Arrange
+        String actualOne = Algorithms.reverse2(testCaseOne);
+        String actualTwo = Algorithms.reverse2(testCaseTwo);
+        String actualThree = Algorithms.reverse2(testCaseThree);
+        String actualFour = Algorithms.reverse2(testCaseFour);
+        String actualFIve = Algorithms.reverse2(testCaseFive);
+
+        // Assert
+        assertEquals(expectedOne, actualOne);
+        assertEquals(expectedTwo, actualTwo);
+        assertEquals(expectedThree, actualThree);
+        assertEquals(expectedFour, actualFour);
+        assertEquals(expectedFive, actualFIve);
+    }
+}
