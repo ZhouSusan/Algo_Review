882ca655d4101efc4aaaadc94945dcaf9e75ab95
Susan Zhou <szhou089@gmail.com>
2023-06-22 23:13:03 -0700

    adding usAtEvenIndex function if char exists at an even index

---
+++ Ex_Files_Java_Algorithms/Exercise Files/01/01_05/start/Algorithms/src/AlgorithmsTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlgorithmsTest {
+
+    @org.junit.jupiter.api.Test
+    void isAtEvenIndex() {
+        // Act
+        String testCaseOne = "HeLLo";
+        String testCaseTwo = "";
+        String testCaseThree = null;
+
+        // Arrange
+        boolean actualOne = Algorithms.isAtEvenIndex(testCaseOne, 'L');
+        boolean actualTwo = Algorithms.isAtEvenIndex(testCaseOne, 'e');
+        boolean actualThree = Algorithms.isAtEvenIndex(testCaseOne, 'T');
+        boolean actualFour = Algorithms.isAtEvenIndex(testCaseOne, 'H');
+        boolean actualFive = Algorithms.isAtEvenIndex(testCaseTwo, 'L');
+        boolean actualSix = Algorithms.isAtEvenIndex(testCaseThree, 'o');
+
+        // Assert
+        assertTrue(actualOne);
+        assertFalse(actualTwo);
+        assertFalse(actualThree);
+        assertTrue(actualFour);
+        assertFalse(actualFive);
+        assertFalse(actualSix);
+    }
+}
