1e4fb19539d4187b41a6e7164f879494c5183c79
Susan Zhou <szhou089@gmail.com>
2023-06-22 22:45:06 -0700

    adding parseContents function that loops through string

--- Ex_Files_Java_Algorithms/Exercise Files/01/01_04/start/Algorithms/src/Algorithms.java
+++ Ex_Files_Java_Algorithms/Exercise Files/01/01_04/start/Algorithms/src/Algorithms.java
@@ -1,6 +1,18 @@
 public class Algorithms {
+    public static void parseContents(String s) {
+        for (char c: s.toCharArray()) {
+            System.out.print(c);
+        }
 
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            System.out.print(c);
+        }
+    }
     public static void main(String[] args) {
         String s = "HeLLo";
+        System.out.println(s.contains("LL"));
+
+        parseContents(s);
     }
 }
