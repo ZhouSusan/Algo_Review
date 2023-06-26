6025666ec943b53d218fe1fb294f457e9224dbd2
Susan Zhou <szhou089@gmail.com>
2023-06-26 16:42:16 -0700

    adding LinkedList algorithm using built-in function

--- Ex_Files_Java_Algorithms/Exercise Files/03/03_02/start/Algorithms/src/Algorithms.java
+++ Ex_Files_Java_Algorithms/Exercise Files/03/03_02/start/Algorithms/src/Algorithms.java
@@ -1,6 +1,26 @@
+import java.util.LinkedList;
+
 public class Algorithms {
 
     public static void main(String[] args) {
+        LinkedList<String> listy = new LinkedList<>();
+        listy.add("Sally");
+        listy.add("Becky");
+        listy.add("Nick");
+        listy.add("Jack");
+        listy.add("Rachel");
+        listy.add("Mark");
 
+        System.out.println(listy.contains("Becky"));
+        System.out.println(listy.size());
+
+        listy.removeFirst();
+        listy.removeLast();
+
+//        for (String item: listy) {
+//            System.out.print(item + "->");
+//        }
+
+        listy.forEach(x -> System.out.print(x + "->"));
     }
 }
