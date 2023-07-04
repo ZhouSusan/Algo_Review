efea927484b436dfa433fd72ab699f549f6ff6d6
Susan Zhou <szhou089@gmail.com>
2023-07-04 15:32:49 -0700

    adding functions that check if the string is upper or lowercase

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/01/01_01/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/01/01_01/start/Algorithms/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Linq;
 
 namespace Algorithms
 {
     class Program
     {
+
+        static Boolean IsUppercase(string s)
+        {
+            return s.All(char.IsUpper);
+        }
+
+        static Boolean IsLowerCase(string s)
+        {
+            return s.All(char.IsLower);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine(IsUppercase("hello"));
+            Console.WriteLine(IsUppercase("HELLO"));
+
+            Console.WriteLine(IsLowerCase("HELLO"));
+            Console.WriteLine(IsLowerCase("hello"));
         }
 
     }
