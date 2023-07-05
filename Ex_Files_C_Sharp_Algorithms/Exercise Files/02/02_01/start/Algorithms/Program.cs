2a0fbbf35ca40b3bad7babc6ea31cc4fbaf6ef74
Susan Zhou <szhou089@gmail.com>
2023-07-04 19:27:50 -0700

    adding Linear Search array algorithm

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/02/02_01/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/02/02_01/start/Algorithms/Program.cs
@@ -3,13 +3,35 @@
 namespace Algorithms {
     class Program {
 
-        static Boolean LinearSearch(int[] input, int n) {
-            return false;
+        static int? LinearSearch(int[] input, int n) {
+            if (input == null || input.Length == 0)
+            {
+                return null;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == n)
+                {
+                    return i+1;
+                }
+            }
+            
+            return null;
         }
 
         static void Main(string[] args) {
             int[] arr = {1, 2, 3, 4, 5, 6 };
-            LinearSearch(arr, 4);
+            Console.WriteLine(LinearSearch(arr, 4));
+            Console.WriteLine(LinearSearch(arr, 8));
+
+            int item = Array.Find(arr, element => element == 4);
+            int item2 = Array.Find(arr, elem => elem == 100);
+            Console.WriteLine(item);
+            Console.WriteLine(item2);
+
+            int[] greaterThanFiveArr = Array.FindAll(arr, n => n >= 5);
+            Array.ForEach(greaterThanFiveArr, Console.WriteLine);
         }
     }
 }
