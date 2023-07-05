eb5da0ab454896e70c2fb52b7cb4520bd7133089
Susan Zhou <szhou089@gmail.com>
2023-07-05 16:58:47 -0700

    practing built-in operations for linked list

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/03/03_02/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/03/03_02/start/Algorithms/Program.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Algorithms {
     class Program {
 
         static void Main(string[] args) {
-            Console.WriteLine("Hello World");
+            LinkedList<string> listOne = new LinkedList<string>();
+            // Add Last
+            // Add Frist
+            listOne.AddLast("Sarah");
+            listOne.AddLast("Sierra");
+            listOne.AddLast("Ciara");
+            listOne.AddLast("Becky");
+            listOne.AddLast("Jess");
+
+            Console.WriteLine(listOne.Contains("Jess"));
+            Console.WriteLine(listOne.Count);
+
+            listOne.RemoveFirst();
+            
+            foreach (string item in listOne)
+            {
+                Console.Write(item + "->");
+            }
+           
         }
     }
 }
