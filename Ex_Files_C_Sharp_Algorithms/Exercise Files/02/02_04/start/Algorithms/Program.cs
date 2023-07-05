f4bcf8cd4428a546755d7b2f7fa4704a228880aa
Susan Zhou <szhou089@gmail.com>
2023-07-04 21:44:47 -0700

    adding Reverse function that returns a reversed input array

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/02/02_04/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/02/02_04/start/Algorithms/Program.cs
@@ -3,12 +3,35 @@
 namespace Algorithms {
     class Program {
         static int[] Reverse(int[] input) {
-            return input;
+
+            if (input == null)
+            {
+                return new int[0]; 
+            }
+
+            if (input.Length == 0 || input.Length == 1)
+            {
+                return input;
+            }
+
+            int[] reversed = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                reversed[i] = input[input.Length-1-i];
+            }
+            return reversed;
         }
 
         static void Main(string[] args) {
             int[] arr = { 1, 2, 3, 4, 5, 6 };
+            int[] arr2 = {-15};
+            int[] arr3 = {};
+            int[] arr4 = null;
+
             Array.ForEach(Reverse(arr), Console.WriteLine);
+            Array.ForEach(Reverse(arr2), Console.WriteLine);
+            Array.ForEach(Reverse(arr3), Console.WriteLine);
+            Array.ForEach(Reverse(arr4), Console.WriteLine);
         }
     }
 }
