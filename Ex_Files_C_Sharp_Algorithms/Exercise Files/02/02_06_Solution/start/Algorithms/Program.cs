4b7ac8577a154cd4e11cb2ea218d10261d4e6b49
Susan Zhou <szhou089@gmail.com>
2023-07-05 00:00:02 -0700

    adding functions that shifts the values to left or right by one

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/02/02_06_Solution/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/02/02_06_Solution/start/Algorithms/Program.cs
@@ -3,11 +3,40 @@
 namespace Algorithms {
     class Program {
         static void RotateArrayLeft(int[] input) {
+            if (input.Length == 0)
+            {
+                return;
+            }
+
+            int firstElem = input[0];
+            for (int i = 0; i < input.Length-1; i++)
+            {
+                input[i] = input[i+1];
+            }
+
+            input[input.Length - 1] = firstElem;
+        }
+
+        static void RotateArrayRight(int[] input)
+        {
+            if (input.Length == 0 || input.Length == 1)
+            {
+                return;
+            }
+
+            int temp = input[input.Length-1];
+            for (int i = input.Length-1; i > 0 ; i--)
+            {
+                input[i] = input[i-1];
+            }
+
+            input[0] = temp;
         }
 
         static void Main(string[] args) {
             int[] arr = { 1, 2, 3, 4, 5, 6 };
-            RotateArrayLeft(arr);
+            // RotateArrayLeft(arr);
+            RotateArrayRight(arr);
             Array.ForEach(arr, Console.WriteLine);
         }
     }
