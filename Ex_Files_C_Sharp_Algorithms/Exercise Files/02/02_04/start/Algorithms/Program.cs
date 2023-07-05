06a452b362d75805ddfff7d4a0f1a48b93ad1221
Susan Zhou <szhou089@gmail.com>
2023-07-04 22:56:47 -0700

    adding ReverseInPlace function that swaps the values in place

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/02/02_04/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/02/02_04/start/Algorithms/Program.cs
@@ -22,16 +22,44 @@ namespace Algorithms {
             return reversed;
         }
 
+        static void ReverseInPlace(int[] arr)
+        {
+            if (arr.Length == 0 || arr.Length == 1) 
+            {
+                return;
+            }
+
+            int low = 0;
+            int high = arr.Length - 1;
+
+            while (low < high)
+            {
+                int temp = arr[low];
+                arr[low] = arr[high];
+                arr[high] = temp;
+
+                low++;
+                high--;
+            }
+        }
+
         static void Main(string[] args) {
             int[] arr = { 1, 2, 3, 4, 5, 6 };
             int[] arr2 = {-15};
             int[] arr3 = {};
             int[] arr4 = null;
 
-            Array.ForEach(Reverse(arr), Console.WriteLine);
-            Array.ForEach(Reverse(arr2), Console.WriteLine);
-            Array.ForEach(Reverse(arr3), Console.WriteLine);
-            Array.ForEach(Reverse(arr4), Console.WriteLine);
+            // Array.ForEach(Reverse(arr), Console.WriteLine);
+            // Array.ForEach(Reverse(arr2), Console.WriteLine);
+            // Array.ForEach(Reverse(arr3), Console.WriteLine);
+            // Array.ForEach(Reverse(arr4), Console.WriteLine);
+
+            ReverseInPlace(arr);
+            Array.ForEach(arr, Console.WriteLine);
+            ReverseInPlace(arr2);
+            Array.ForEach(arr2, Console.WriteLine);
+            ReverseInPlace(arr3);
+            Array.ForEach(arr3, Console.WriteLine);
         }
     }
 }
