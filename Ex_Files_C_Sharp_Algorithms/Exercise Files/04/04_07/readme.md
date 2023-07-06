c36d6a893711fc0f43df95a56243f976d0a77ae6
Susan Zhou <szhou089@gmail.com>
2023-07-05 21:00:21 -0700

    adding function that prints the next greater value

---
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/04/04_07/readme.md
@@ -0,0 +1,21 @@
+Challenge: Next greater element
+
+Print the next greater element for every element in the array.
+
+Note: The next greater element is the first larger element on the right side of the array. If none found, return -1.
+
+Example:
+
+Input: {4, 5, 2, 35}
+
+Output:
+
+    4-> 5
+    5 -> 35
+    2 -> 35
+    35 -> -1
+Input: {2}
+
+Output:
+
+    2 -> -1
--- Ex_Files_C_Sharp_Algorithms/Exercise Files/04/04_07/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/04/04_07/start/Algorithms/Program.cs
@@ -4,7 +4,45 @@ using System.Collections.Generic;
 namespace Algorithms {
     class Program {
         static void printNextGreaterElement(int[] arr) {
+            if (arr == null || arr.Length == 0)
+            {
+                return;
+            }
 
+            Stack<int> arrNums = new Stack<int>();
+            arrNums.Push(arr[0]);
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int next = arr[i];
+                if (arrNums.Count > 0)
+                {
+                    int popped = arrNums.Pop();
+
+                    while (popped < next)
+                    {
+                        Console.WriteLine(popped + " --> " + next);
+                        if (arrNums.Count == 0)
+                        {
+                            break;
+                        }
+                        // increment popped
+                        popped = arrNums.Pop();
+                    }
+
+                    if (popped > next)
+                    {
+                        arrNums.Push(popped);
+                    }
+                }
+
+                arrNums.Push(next);
+            }
+
+            while (arrNums.Count > 0)
+            {
+                Console.WriteLine(arrNums.Pop() + " --> " + -1);
+            }
         }
 
         static void Main(string[] args) {
