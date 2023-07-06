c4fb16bb77f56a4230aaf0b928aeafe495f631d4
Susan Zhou <szhou089@gmail.com>
2023-07-05 20:28:55 -0700

    reviewing basic stack operations

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/04/04_05/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/04/04_05/start/Algorithms/Program.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Algorithms {
     class Program {
 
         static void Main(string[] args) {
+            Stack<string> stack = new Stack<string>();
+            Console.WriteLine("Start Main");
+            stack.Push("Main");
+            Console.WriteLine("Start ResponseBuilder");
+            stack.Push("ResponseBuilder");
+            Console.WriteLine("Start CallExternalService");
+            stack.Push("CallExternalService");
+            Console.WriteLine("END " + stack.Pop());
+            Console.WriteLine("Start ParseExternalData");
+            stack.Push("ParseExternalData");
+            Console.WriteLine("END " + stack.Pop());
+            Console.WriteLine("END " + stack.Pop());
+            Console.WriteLine("END " + stack.Pop());
 
+            // stack.Peek() - raises an exception at an empty stack
+            // stack.TryPeek() - check if stack is empty without raising an error
+            string item;
+            Console.WriteLine(stack.TryPeek(out item));
         }
     }
 }
