0d5116408852b7de65013573411b8e843350a748
Susan Zhou <szhou089@gmail.com>
2023-07-07 09:06:06 -0700

    adding method that track elements and how many times it appears

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/05/05_04/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/05/05_04/start/Algorithms/Program.cs
@@ -4,7 +4,56 @@ using System.Collections.Generic;
 namespace Algorithms {
     class Program {
 
+        static Dictionary<int, int> displayFreqOfEachElement(int[] arr)
+        {
+            if (arr == null || arr.Length == 0) return new Dictionary<int, int>();
+
+            Dictionary<int, int> countDictionary = new Dictionary<int, int>();
+
+            foreach (int num in arr)
+            {
+                if (countDictionary.ContainsKey(num))
+                {
+                    countDictionary[num]++;
+                } 
+                else
+                {
+                    countDictionary.Add(num, 1);
+                }
+            }
+
+            foreach(KeyValuePair<int, int> x in countDictionary)
+            {
+                Console.WriteLine(x.Key + " : " + x.Value);
+            }
+
+            return countDictionary; 
+        } 
+
         static void Main(string[] args) {
+
+            int[] arr1 = new int[] { 1, 2, 3, 2, 4, 3, 5, 2 };
+            displayFreqOfEachElement(arr1);
+
+            Console.WriteLine();
+
+            int[] arr2 = new int[] { 7, 7, 7, 8, -1, -4, -1 };
+            displayFreqOfEachElement(arr2);
+
+            Console.WriteLine();
+
+            int[] arr3 = new int[] {};
+            displayFreqOfEachElement(arr3);
+
+            Console.WriteLine();
+
+            int[] arr4= null;
+            displayFreqOfEachElement(arr4);
+
+            Console.WriteLine();
+
+            int[] arr5 = new int[] { 1 };
+            displayFreqOfEachElement(arr5);
         }
     }
 }
--- Ex_Files_C_Sharp_Algorithms/Exercise Files/05/05_04/start/Algorithms/readme.md
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/05/05_04/start/Algorithms/readme.md
@@ -1,3 +1,3 @@
 ﻿Challenge: Display the count of each element in an unsorted array.
 
-<key = element, value = count of element>
+<key = element, value = count of element> {5, 7, 3, 0, 2}
