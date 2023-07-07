be7746862f6a588c16209f529878bcfd7e4212e1
Susan Zhou <szhou089@gmail.com>
2023-07-07 07:24:13 -0700

    adding a binary search tree

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/06/06_02/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/06/06_02/start/Algorithms/Program.cs
@@ -1,10 +1,52 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Algorithms {
 
+    class Node
+    {
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public int Data { get; set; }
+    }
+
+    class BinarySearchTree
+    {
+        public static Node Insert(Node root, int val)
+        {
+            if (root == null)
+            {
+                root = new Node();
+                root.Data = val;
+                return root;
+            }
+           
+            if (val < root.Data)
+            {
+                // insert on left
+                root.Left = Insert(root.Left, val);
+            }
+            else if (val > root.Data)
+            {
+                // insert on right
+                root.Right = Insert(root.Right, val);
+            }
+
+            return root;
+        }
+    }
+
     class Program {
         static void Main(string[] args) {
-            
+
+            Node rootNode = new Node();
+            rootNode.Data = 4;
+            BinarySearchTree.Insert(rootNode, 2);
+            BinarySearchTree.Insert(rootNode, 3);
+            BinarySearchTree.Insert(rootNode, 5);
+            BinarySearchTree.Insert(rootNode, 6);
+            BinarySearchTree.Insert(rootNode, 4);
+
         }
     }
 }
