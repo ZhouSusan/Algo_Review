8e312de1df914cc04a03288dc1d44460041de751
Susan Zhou <szhou089@gmail.com>
2023-07-06 21:07:44 -0700

    adding standard dictionary and hashset operations

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/05/05_02/start/Algorithms/Employee.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/05/05_02/start/Algorithms/Employee.cs
@@ -18,6 +18,29 @@ namespace Algorithms {
             Employee employee = new Employee("Robby", 3827, "Technology");
             Employee employee2 = new Employee("Bobby", 9415, "Marketing");
             Employee employee3 = new Employee("Sally", 2519, "Sales");
+
+            Dictionary<int, Employee> employeesById = new Dictionary<int, Employee>();
+            employeesById.Add(employee.id, employee);
+            employeesById.Add(employee2.id, employee2);
+            employeesById.Add(employee3.id, employee3);
+
+            // if the key exists in the dictionary and it has an employee value, it will be stored in e
+            Employee e;
+            if (employeesById.TryGetValue(9415, out e))
+            {
+                Console.WriteLine(e.name + " : " + e.department);
+            }
+
+            Console.WriteLine(employeesById.ContainsKey(1235));
+            Console.WriteLine(employeesById.ContainsKey(3827));
+
+            HashSet<string> productCodes = new HashSet<string>();
+            productCodes.Add("8F26B");
+            productCodes.Add("0A43P");
+            productCodes.Add("J2H762");
+
+            Console.WriteLine(productCodes.Contains("45HT21"));
+            Console.WriteLine(productCodes.Contains("J2H762"));
         }
     }
 }
