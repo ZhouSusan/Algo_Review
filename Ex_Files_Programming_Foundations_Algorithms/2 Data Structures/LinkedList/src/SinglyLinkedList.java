5456c7e3642f4eb0e0220f46fc4910c08478fbda
Susan Zhou <szhou089@gmail.com>
2023-08-02 00:01:38 -0700

    adding insertNode and isEmpty method in SinglyLinkedList class

--- Ex_Files_Programming_Foundations_Algorithms/2 Data Structures/LinkedList/src/SinglyLinkedList.java
+++ Ex_Files_Programming_Foundations_Algorithms/2 Data Structures/LinkedList/src/SinglyLinkedList.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class SinglyLinkedList {
+public class SinglyLinkedList {
+    Node head;
+    private int count;
+    public SinglyLinkedList() {
+        this.head = head;
+        this.count = 0;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public void insertNode(int data) {
+        Node addThisNode = new Node(data);
+
+        if (isEmpty()) {
+            this.head = addThisNode;
+        }
+
+        addThisNode.setNext(this.head.getData());
+        this.head = addThisNode;
+        this.count++;
+    }
+
+    public boolean isEmpty() {
+        return this.head == null;
+    }
 }
