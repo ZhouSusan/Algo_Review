5f248304eb5829b9925148d0070df6f589d5e9f5
Susan Zhou <szhou089@gmail.com>
2023-08-07 22:08:10 -0700

    adding power and factorial recursive functions

--- Ex_Files_Programming_Foundations_Algorithms/3 Recursion/src/Main.java
+++ Ex_Files_Programming_Foundations_Algorithms/3 Recursion/src/Main.java
@@ -9,7 +9,27 @@ public class Main {
         countDown(x-1);
     }
 
+    public static int power(int num, int pwr) {
+        if (pwr == 0) {
+            return 1;
+        }
+
+        return num * power(num, pwr-1);
+    }
+
+    public static int factorial(int num) {
+        if (num == 0) {
+            return 1;
+        }
+
+        return num * factorial(num-1);
+    }
+
     public static void main(String[] args) {
         countDown(5);
+        System.out.printf("%d to the power of %d is %s.\n", 5, 3, power(5, 3));
+        System.out.printf("%d to the power of %d is %s.\n", 1, 5, power(1, 5));
+        System.out.printf("%d! is %s.\n", 4, factorial(4));
+        System.out.printf("%d! is %s.", 0, factorial(0));;
     }
 }
