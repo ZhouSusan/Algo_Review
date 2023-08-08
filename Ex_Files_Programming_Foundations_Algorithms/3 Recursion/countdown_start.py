f984ca595fb7c6e618b7b0f9ee66cc587e1afb54
Susan Zhou <szhou089@gmail.com>
2023-08-07 22:10:17 -0700

    adding exercise solution for recursion 's section

--- Ex_Files_Programming_Foundations_Algorithms/3 Recursion/countdown_start.py
+++ Ex_Files_Programming_Foundations_Algorithms/3 Recursion/countdown_start.py
@@ -1,8 +1,11 @@
 # use recursion to implement a countdown counter
 
-
 def countdown(x):
-    return
-
+     if x == 0:
+        print("Done!")
+        return
+    else:
+        print(x, "...")
+        countdown(x-1)
 
 countdown(5)
