b132bd21b929fd2c4cbb17391beee0d35f93ce1b
Susan Zhou <szhou089@gmail.com>
2023-08-15 01:41:07 -0700

    adding binarySearch algorithm

--- Ex_Files_Programming_Foundations_Algorithms/5 Searching/src/Searching.java
+++ Ex_Files_Programming_Foundations_Algorithms/5 Searching/src/Searching.java
@@ -15,10 +15,41 @@ public class Searching {
         }
         return -1;
     }
-    
+
+    // searching for an item in an ordered list
+    //# this technique uses a binary search
+
+    public static int binarySearch(int[] arr, int item) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+
+        int low = 0;
+        int high = arr.length-1;
+        int middle;
+
+        while (low <= high) {
+            middle = low + (high-low)/2;
+            if (arr[middle] == item) {
+                return middle;
+            } else if ( item < arr[middle]) {
+                high = middle - 1;
+            } else {
+                low = middle + 1;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] itemArr = new int[] {6, 20, 8, 19, 56, 23, 87, 41, 49, 53};
         System.out.println(linearSearch(itemArr, 87));
         System.out.println(linearSearch(itemArr, 250));
+
+        int[] itemArr2 = new int[] {6, 20, 28, 39, 56, 63, 67, 74, 81};
+        System.out.println(binarySearch(itemArr2, 67));
+        System.out.println(binarySearch(itemArr2, 81));
+        System.out.println(binarySearch(itemArr2, 41));
     }
 }
--- Ex_Files_Programming_Foundations_Algorithms/5 Searching/src/SearchingTest.java
+++ Ex_Files_Programming_Foundations_Algorithms/5 Searching/src/SearchingTest.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SearchingTest {
 
@@ -20,5 +21,47 @@ class SearchingTest {
         assertEquals(expectedOne, actualOne);
         assertEquals(expectedTwo, actualTwo);
         assertEquals(expectedThree, actualThree);
+    }
+
+    @org.junit.jupiter.api.Test
+    void binarySearchTest() {
+        // Arrange
+        int[] testCaseOne = new int[] {-10, -6, 0, 8, 19, 56, 23, 87, 49, 53};
+        int[] testCaseTwo = new int[] {0, 10, 20, 30, 40};
+        int[] testCaseThree = new int[] {};
+        int[] testCaseFour = null;
+
+        int expectedOne = 2;
+        int expectedTwo = 4;
+        int expectedThree = 7;
+        int expectedFour = 0;
+        int expectedFive = -1;
+        int expectedSix = 4;
+        int expectedSeven = -1;
+        int expectedEight = -1;
+
+        // Act
+        int actualOne = Searching.binarySearch(testCaseOne, 0);
+        int actualTwo = Searching.binarySearch(testCaseOne, 19);
+        int actualThree = Searching.binarySearch(testCaseOne, 87);
+        int actualFour = Searching.binarySearch(testCaseTwo, 0);
+        int actualFive = Searching.binarySearch(testCaseTwo, 50);
+        int actualSix = Searching.binarySearch(testCaseTwo, 40);
+        int actualSeven = Searching.binarySearch(testCaseThree, 2);
+        int actualEight = Searching.binarySearch(testCaseFour, 4);
+
+        // Assert
+        assertNotNull(actualOne);
+        assertEquals(expectedOne, actualOne);
+        assertEquals(expectedTwo, actualTwo);
+        assertEquals(expectedThree, actualThree);
+
+        assertNotNull(actualFour);
+        assertEquals(expectedFour, actualFour);
+        assertEquals(expectedFive, actualFive);
+        assertEquals(expectedSix, actualSix);
+
+        assertEquals(expectedSeven, actualSeven);
+        assertEquals(expectedEight, actualEight);
     }
 }
