d781f1781fb46265452348cd7a79990a61d4455d
Susan Zhou <szhou089@gmail.com>
2023-08-23 23:54:31 -0700

    adding algorithm that determines if two strings are equal after backspace is processed

--- Strings/StringComparator/src/StringComparator.java
+++ Strings/StringComparator/src/StringComparator.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class StringComparator {
 
     /**
@@ -19,6 +21,57 @@ public class StringComparator {
 
         return true;
     }
+
+    /**
+     * Determines if the given strings are equal after the backspace characters '#' are processed.
+     * @param s - string
+     * @param t - string
+     * @return boolean - are the two strings equal after backspace characters '#" are processed
+     */
+    public static Boolean backspaceStringCompare(String s, String t) {
+        if (s == null && t == null) {
+            return true;
+        }
+
+        Stack<Character> charStackS = new Stack<Character>();
+        Stack<Character> charStackT = new Stack<Character>();
+
+        if (s == null || t == null || (charStackS.size() != charStackT.size())) {
+            return false;
+        }
+
+        if (s.isEmpty() && t.isEmpty()) {
+            return true;
+        }
+
+        int i = 0; // index counter for s
+        while (i < s.length()) {
+            char currentChar = s.charAt(i);
+            if (Character.isLetter(currentChar)) {
+                charStackS.add(currentChar);
+            } else if (currentChar == '#') {
+                if (charStackS.size() > 0) {
+                    charStackS.pop();
+                }
+            }
+            i++;
+        }
+
+        int j = 0; // index counter for t
+        while (j < t.length()) {
+            char currentChar = t.charAt(j);
+            if (Character.isLetter(currentChar)) {
+                charStackT.add(currentChar);
+            } else if (currentChar == '#') {
+                if (charStackT.size() > 0) {
+                    charStackT.pop();
+                }
+            }
+            j++;
+        }
+
+        return charStackS.equals(charStackT);
+    }
     public static void main(String[] args) {
         String strA = "AbCdE";
         String strB = "abcde";
@@ -26,5 +79,21 @@ public class StringComparator {
 
         System.out.println(caseInsensitiveStringCompare(strA, strB));
         System.out.println(caseInsensitiveStringCompare(strA, strC));
+
+        String S1 = "ab#c";
+        String T1 = "ad#c";
+        System.out.println(backspaceStringCompare(S1, T1));
+
+        String S2 = "ab##";
+        String T2 = "c#d#";
+        System.out.println(backspaceStringCompare(S2, T2));
+
+        String S3 = "a##c";
+        String T3 = "#a#c";
+        System.out.println(backspaceStringCompare(S3, T3));
+
+        String S4 = "a#c";
+        String T4 = "b";
+        System.out.println(backspaceStringCompare(S4, T4));
     }
 }
--- Strings/StringComparator/src/StringComparatorTest.java
+++ Strings/StringComparator/src/StringComparatorTest.java
@@ -37,4 +37,60 @@ class StringComparatorTest {
         assertEquals(expectedFive, actualFive);
         assertEquals(expectedSix, actualSix);
     }
+
+    @org.junit.jupiter.api.Test
+    void backspaceStringCompare() {
+        // Arrange
+        String testCaseS1 = "ab#c";
+        String testCaseT1 = "ad#c";
+        Boolean expectedOne = true;
+
+        String testCaseS2 = "ab##";
+        String testCaseT2 = "c#d#";
+        Boolean expectedTwo = true;
+
+        String testCaseS3 = "a##c";
+        String testCaseT3 = "#a#c";
+        Boolean expectedThree = true;
+
+        String testCaseS4 = "a#c";
+        String testCaseT4 = "b";
+        Boolean expectedFour = false;
+
+        String testCaseS5 = "";
+        String testCaseT5 = "";
+        Boolean expectedFive = true;
+
+        String testCaseS6 = "a#c#e";
+        String testCaseT6 = null;
+        Boolean expectedSix = false;
+
+        String testCaseS7 = null;
+        String testCaseT7 = "bc";
+        Boolean expectedSeven = false;
+
+        String testCaseS8 = null;
+        String testCaseT8 = null;
+        Boolean expectedEight = true;
+
+        // Act
+        Boolean actualOne = StringComparator.backspaceStringCompare(testCaseS1, testCaseT1);
+        Boolean actualTwo = StringComparator.backspaceStringCompare(testCaseS2, testCaseT2);
+        Boolean actualThree = StringComparator.backspaceStringCompare(testCaseS3, testCaseT3);
+        Boolean actualFour = StringComparator.backspaceStringCompare(testCaseS4, testCaseT4);
+        Boolean actualFive = StringComparator.backspaceStringCompare(testCaseS5, testCaseT5);
+        Boolean actualSix = StringComparator.backspaceStringCompare(testCaseS6, testCaseT6);
+        Boolean actualSeven = StringComparator.backspaceStringCompare(testCaseS7, testCaseT7);
+        Boolean actualEight = StringComparator.backspaceStringCompare(testCaseS8, testCaseT8);
+
+        // Assert
+        assertEquals(expectedOne, actualOne);
+        assertEquals(expectedTwo, actualTwo);
+        assertEquals(expectedThree, actualThree);
+        assertEquals(expectedFour, actualFour);
+        assertEquals(expectedFive, actualFive);
+        assertEquals(expectedSix, actualSix);
+        assertEquals(expectedSeven, actualSeven);
+        assertEquals(expectedEight, actualEight);
+    }
 }
