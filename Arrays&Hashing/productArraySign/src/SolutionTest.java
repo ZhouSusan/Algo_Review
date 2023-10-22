8988721a626ce2c9f66f30b2e009b1a9106d6fdc
Susan Zhou <susan.0.zhou@gmail.com>
2023-10-22 12:27:39 -0700

    updating a few test cases

--- Arrays&Hashing/productArraySign/src/SolutionTest.java
+++ Arrays&Hashing/productArraySign/src/SolutionTest.java
@@ -20,14 +20,17 @@ class SolutionTest {
         int[] testCase5 = {1,5,0,2,-3};
         int expected5 = 0;
 
-        int[] testCase6 = {-6,-4};
+        int[] testCase6 = {9,72,-2,34,29,-49,-22,-77,-17,-66,-75,-44,-30,-24};
         int expected6 = 1;
 
-        int[] testCase7 = {-1,9};
+        int[] testCase7 = {41,65,14,80,20,10,55,58,24,56,28,86,96,10,3,84,4,41,13,32,42,43,83,78,82,70,15,-41};
         int expected7 = -1;
 
         int[] testCase8 = {3};
         int expected8 = 1;
+
+        int[] testCase9 = {0,1,2,3};
+        int expected9 = 0;
 
         // Act
         int actual1 = Solution.arraySign(testCase1);
@@ -38,6 +41,7 @@ class SolutionTest {
         int actual6 = Solution.arraySign(testCase6);
         int actual7 = Solution.arraySign(testCase7);
         int actual8 = Solution.arraySign(testCase8);
+        int actual9 = Solution.arraySign(testCase9);
 
         //Assert
         assertEquals(expected1, actual1);
@@ -48,5 +52,6 @@ class SolutionTest {
         assertEquals(expected6, actual6);
         assertEquals(expected7, actual7);
         assertEquals(expected8, actual8);
+        assertEquals(expected9, actual9);
     }
 }
