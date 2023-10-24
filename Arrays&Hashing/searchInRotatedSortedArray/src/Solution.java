637194f9e96ac55ea4743bea51e5ed7b44bf5fe3
Susan Zhou <susan.0.zhou@gmail.com>
2023-10-23 22:15:43 -0700

    updating search method to O(log n) runtime

--- Arrays&Hashing/searchInRotatedSortedArray/src/Solution.java
+++ Arrays&Hashing/searchInRotatedSortedArray/src/Solution.java
@@ -1,4 +1,5 @@
 public class Solution {
+
     /**
      * Searching for a target value in a rotated sorted array.
      * @param nums<Integer> - an integer array, sorted in ascending order with distinct values
@@ -6,15 +7,34 @@ public class Solution {
      * @return  integer - index of target if it is in nums, or -1
      */
     public static int search(int[] nums, int target) {
-        int idx = -1;
+        int low = 0;
+        int high = nums.length-1;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                idx = i;
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if(nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
-        return idx;
+
+        return -1;
     }
+
     public static void main(String[] args) {
         int[] nums = {4,5,6,7,0,1,2};
         int target = 1;
