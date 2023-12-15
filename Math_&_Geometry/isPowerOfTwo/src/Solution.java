f2a5f9512d5d354d9b4aa825e527d5f10f524938
Susan Zhou <susan.0.zhou@gmail.com>
2023-12-14 23:31:51 -0800

    refactoring initial solution and practicing bit manipulation

--- Math_&_Geometry/isPowerOfTwo/src/Solution.java
+++ Math_&_Geometry/isPowerOfTwo/src/Solution.java
@@ -6,10 +6,8 @@ public class Solution {
      */
     public static boolean isPowerOfTwo(int n) {
         if (n == 1) return true;
-
-        if (n <= 0 || n % 2 == 1) return false;
-
-        return isPowerOfTwo(n/2);
+        if (n <= 0) return false;
+        return ( n & (n-1)) == 0;
     }
 
     public static void main(String[] args) {
