a88def032d82179b33ed2f621142b3b71225e797
Susan Zhou <susan.0.zhou@gmail.com>
2024-01-17 22:50:11 -0800

    adding hackerrank challenge - Java 1D Array

---
+++ Arrays&Hashing/java1DArray/src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            int currentNum = scan.nextInt();
+            a[i] = currentNum;
+        }
+
+        scan.close();
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
